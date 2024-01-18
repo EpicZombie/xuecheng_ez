6ddb5a5ab112f5bed27c8e714572ad4a6182f732
estar <yjl761152401@163.com>
2024-01-18 11:44:55 +0800

    文件结构的修改

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -0,0 +1,38 @@
+package com.xuecheng.media.service;
+
+import com.xuecheng.base.exception.RestErrorResponse;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.base.model.RestResponse;
+import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
+import com.xuecheng.media.model.po.MediaFiles;
+
+/**
+ * @description 媒资文件管理业务类
+ * @author Mr.M
+ * @date 2022/9/10 8:55
+ * @version 1.0
+ */
+public interface MediaFileService {
+
+ /**
+  * @description 媒资文件查询方法
+  * @param pageParams 分页参数
+  * @param queryMediaParamsDto 查询条件
+  * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
+  * @author Mr.M
+  * @date 2022/9/10 8:57
+ */
+ public PageResult<MediaFiles> queryMediaFiels(Long companyId,PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+
+ public UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, String localFilePath);
+
+ public RestResponse mergechunks(Long companyId, String fileMd5, int chunkTotal, UploadFileParamsDto uploadFileParamsDto);
+
+ public RestResponse checkFile(String fileMd5);
+
+ public RestResponse checkChunk(String fileMd5, int chunkIndex);
+
+}
